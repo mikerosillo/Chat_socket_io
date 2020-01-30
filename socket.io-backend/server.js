595c6a670bdcaa091a436e3a18f1aff59b7661c2
Miguel Rosillo <miguel.rosillo@yaydoo.com>
2020-01-30 11:43:30 -0600

    method to send backe the message event to socket implemented in server js file

--- socket.io-backend/server.js
+++ socket.io-backend/server.js
@@ -1,7 +1,8 @@
 const io = require("socket.io")();
 io.on("connection", socket => {
-    socket.on('message', message => {
+    socket.on("message", message => {
         console.log(message);
+        io.emit("message", message); // this is goin to send the message event to all the sockets that is on the server
     })
     console.log("user connected")
 })
